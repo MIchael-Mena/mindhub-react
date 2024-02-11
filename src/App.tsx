29e9fda4587fd8e21b23894b36c8d33ae878570a
Michael Mena <michael.mena2408@gmail.com>
2024-02-11 15:10:22 -0300

    delete console.log

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,6 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { SnackbarConfig } from './config/snackbar.config';
 
 function App() {
-  console.log(
-    'App',
-    import.meta.env.VITE_BASE_URL,
-    import.meta.env.MODE,
-    import.meta.env.VITE_GOOGLE_CLIENT_ID
-  );
   return (
     <React.Fragment>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { ThemeConfig } from './config/theme.config.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import './index.css';
 import 'dayjs/locale/en';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // El strict mode puede causar que useState se ejecute dos veces
+  // El strict mode ayuda a detectar problemas en la aplicacion en modo desarrollo
   <React.StrictMode>
     <Provider store={store}>
       <ThemeConfig>
